8ffa8a88f599f182932ec6a66b7bd33947491051
Cindy Le <cindyle106@gmail.com>
2024-09-18 19:05:24 -0600

    post to create route

--- server.js
+++ server.js
@@ -16,12 +16,37 @@ const morgan = require("morgan");
 //Connection 
 mongoose.connect(process.env.MONGODB_URI);
 
-
+//Configure View Engine 
+app.set("view engine","ejs");
 //Routes 
 
+//Landing Page
+app.get("/sonnyangels", (req, res) => {
+    res.render("index");
+  });
+
+//Render a new page 
 app.get("/sonnyangels/new", (req, res) => {
     res.render("sonnyangels/new");
   });
+
+//CREATE ROUTE - app.post 
+app.post("/sonnyangels", async (req, res) => {
+    if (req.body.isPulled) {
+      req.body.isPulled = true;
+    } else {
+      req.body.isPulled = false;
+    }
+  
+    try {
+      await sonnyangel.create(req.body);
+      res.redirect("/sonnyangels");
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+
+
 //app.listen 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
